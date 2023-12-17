e94e98f3ab6864a62e7370f122f3305f2c833d7f
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-12-17 18:31:32 +0000

    Fase 2 - Final
    Fase 2 Final, sem relatório
    atualização do Program.cs (try catch)

--- ProjetoAutoprem/AutoPrem/AutoPrem.App/Program.cs
+++ ProjetoAutoprem/AutoPrem/AutoPrem.App/Program.cs
@@ -27,29 +27,95 @@ class Program
         Moto moto = new Moto(3);
         Funcionario funcionario = new Funcionario("João");
 
-        // Iniciando exemplos de utilização:
+        // Lidando com exceções ao ligar os veículos
+        try
+        {
+            veiculo.Ligar();
+            ioService.WriteLine($"Veículo {veiculo.ID} ligado com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao ligar veículo {veiculo.ID}: {ex.Message}");
+        }
 
-        // Ligando os veículos:
-        veiculo.Ligar();
-        carro.Ligar();
-        moto.Ligar();
+        try
+        {
+            carro.Ligar();
+            ioService.WriteLine($"Carro {carro.ID} ligado com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao ligar carro {carro.ID}: {ex.Message}");
+        }
 
-        // Desligando os veículos:
-        veiculo.Desligar();
-        carro.Desligar();
-        moto.Desligar();
+        try
+        {
+            moto.Ligar();
+            ioService.WriteLine($"Moto {moto.ID} ligada com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao ligar moto {moto.ID}: {ex.Message}");
+        }
+
+        // Lidando com exceções ao desligar os veículos
+        try
+        {
+            veiculo.Desligar();
+            ioService.WriteLine($"Veículo {veiculo.ID} desligado com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao desligar veículo {veiculo.ID}: {ex.Message}");
+        }
+
+        try
+        {
+            carro.Desligar();
+            ioService.WriteLine($"Carro {carro.ID} desligado com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao desligar carro {carro.ID}: {ex.Message}");
+        }
+
+        try
+        {
+            moto.Desligar();
+            ioService.WriteLine($"Moto {moto.ID} desligada com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao desligar moto {moto.ID}: {ex.Message}");
+        }
         #endregion
 
         #region Serviços e Tarefas
         // Exemplo de utilização de serviços e tarefas
-        // Exemplo de Serviço de Manutenção:
-        ServicoManutencao servico = new ServicoManutencao(DateTime.Now, "Manutenção de rotina");
-        servico.RealizarManutencao(new ComponenteEspecifico());
+        // Serviço de Manutenção
+        try
+        {
+            ServicoManutencao servico = new ServicoManutencao(DateTime.Now, "Manutenção de rotina");
+            servico.RealizarManutencao(new ComponenteEspecifico());
+            ioService.WriteLine("Manutenção realizada com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao realizar manutenção: {ex.Message}");
+        }
 
-        // Exemplo de Tarefa do Funcionário:
-        funcionario.RealizarTarefa(new ComponenteEspecifico());
+        // Tarefa do Funcionário
+        try
+        {
+            bool sucessoTarefa = funcionario.RealizarTarefa(new ComponenteEspecifico());
+            ioService.WriteLine(sucessoTarefa ? "Tarefa realizada com sucesso." : "Erro ao realizar tarefa.");
+        }
+        catch (Exception ex)
+        {
+            ioService.WriteLine($"Erro ao realizar tarefa: {ex.Message}");
+        }
         #endregion
 
-        
+        // Adicione mais funcionalidades conforme necessário
     }
 }
